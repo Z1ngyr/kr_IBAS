c73d9700eaf09231d23bcf80ad4627687db17b59
Z1ngyr <63844844+Z1ngyr@users.noreply.github.com>
2021-10-04 21:27:48 +0500

    Add files via upload

---
+++ zadanie7.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Vvedi epsilon" << endl;;
+	double e;
+	cin >> e;
+	double x;
+	cout << "Vvedi x" << endl;;
+	cin >> x;
+	double a1 = x;
+	double a2 = 5 * a1 + x * x * x / (3 * a1 - 1);
+	
+	while (abs(a2-a1)>=e) {
+		a1 = a2;
+		a2= 5 * a1 + x * x * x / (3 * a1 - 1);
+		
+	}
+	cout << a2;
+}
