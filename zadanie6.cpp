f65b2f6be7c51b0f916df1251d4ab93cb9909fb2
Z1ngyr <63844844+Z1ngyr@users.noreply.github.com>
2021-10-04 20:48:51 +0500

    Add files via upload

---
+++ zadanie6.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    double S = 0;
+    for (int k = 1;k <= n;k++) {
+        double z = 0;
+        for (int i = 1;i <= k;i++) {
+            z += pow(i + 3, 2);
+        }
+         S += pow(k, 4)*1/z;
+
+    }
+    cout << S;
+}
+
