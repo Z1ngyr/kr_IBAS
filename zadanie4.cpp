b75d8a745e66fb24d11c469c9a09cf011d8d53e9
Z1ngyr <63844844+Z1ngyr@users.noreply.github.com>
2021-10-04 20:37:48 +0500

    Add files via upload

---
+++ zadanie4.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    double c = 2.1;
+    //b0-больше нуля m0-меньше нуля
+    int b0=0, m0=0;
+    while (c <= 4.2)
+    {
+        double z = pow(sin(abs(c * pow(x, 3) + x * x)), 2) / (sqrt(pow(c * x * x - pow(x, 3), 2) - 6.14));
+        cout << c <<" " <<z<<endl;
+        if (z > 0)b0++;
+        else m0++;
+        c += 0.2;
+    }
+    cout << b0 <<" " << m0;
+}
+
