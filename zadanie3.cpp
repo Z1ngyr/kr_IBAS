a55af5192171d6a9896fbf59cb736a87d08ee4e0
Z1ngyr <63844844+Z1ngyr@users.noreply.github.com>
2021-10-04 20:20:20 +0500

    Add files via upload

---
+++ zadanie3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >>  x;
+    if (x <= 4)
+        cout << exp(-3 * x) - pow(2 - x, 1 / 3);
+    else if (x <= 7)
+        cout << x / (x - 6);
+    else cout << pow(x, 3) + 9;
+
+     
+
+
+}
+
+
